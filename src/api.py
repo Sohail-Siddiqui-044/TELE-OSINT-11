1d6ce0ecccf4c9e2dcf144f8b817f43ed54ab33b
Sohail Siddiqui <176403049+Sohail-Siddiqui-044@users.noreply.github.com>
2026-08-19 13:59:21 +0530

    api.py

--- src/api.py
+++ src/api.py
@@ -1,17 +1,16 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 def ipstackapi():
-    ipstack = "36f8692abc551f6c2939321d937c2a29"
-    return ipstack
+    return os.getenv("IPSTACK_API_KEY")
 
 
 def macvendor():
-    macapi = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJtYWN2ZW5kb3JzIiwiZXhwIjoxODc1NzA1MjcxLCJpYXQiOjE1NjEyMDkyNzEsImlzcyI6Im1hY3ZlbmRvcnMiLCJqdGkiOiI1OGYwNDQyZi1kMjkyLTRlYjQtOTAwYy1jMWYxYWMzZjZlYjQiLCJuYmYiOjE1NjEyMDkyNzAsInN1YiI6IjE2MDMiLCJ0eXAiOiJhY2Nlc3MifQ.evlDM70N0GBK3qT4TF6cAZa7_en6ZaqlpxlarFYaoeTJBB1guNoCWcXjTpRBep80Wy60peEwGIwkx-7cXLqrSQ"
-    return macapi
+    return os.getenv("MACVENDORS_API_KEY")
 
 
 def phoneapis():
-    phoneapi = 'cd3af5f7d1897dc1707c47d05c3759fd'
-    return phoneapi
-
-
+    return os.getenv("PHONE_API_KEY")
